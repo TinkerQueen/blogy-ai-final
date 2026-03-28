18ef4c93afb5230a9ea86148a6a4651738dbc605
TinkerQueen <aashii.sharma31@gmail.com>
2026-03-28 11:30:28 +0530

    updated

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\acer\OneDrive\Desktop\dtu project\DTU-project\venv
+set VIRTUAL_ENV=D:\Hackthon\blogy-ai-backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
